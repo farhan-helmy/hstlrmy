a8ef92087c0e6c31852bf7981643735c58e10483
Farhan Helmy <farhanhlmy@gmail.com>
2023-02-09 12:59:47 +0800

    add about us page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -21,126 +21,20 @@ import useCartStore from '../store/cart'
 import { SideShoppingCart } from './SideShoppingCart';
 import Link from 'next/link';
 import { trpc } from '../utils/trpc';
+import { router } from '../server/trpc/trpc';
+import { useRouter } from 'next/router';
 
 const navigation = {
   categories: [
     {
       id: 'products',
       name: 'Products',
-      featured: [
-        {
-          name: 'New Arrivals',
-          href: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
-          imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
-        },
-        {
-          name: 'Basic Tees',
-          href: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
-          imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
-        },
-      ],
-      sections: [
-        {
-          id: 'clothing',
-          name: 'Clothing',
-          items: [
-            { name: 'Tops', href: '#' },
-            { name: 'Dresses', href: '#' },
-            { name: 'Pants', href: '#' },
-            { name: 'Denim', href: '#' },
-            { name: 'Sweaters', href: '#' },
-            { name: 'T-Shirts', href: '#' },
-            { name: 'Jackets', href: '#' },
-            { name: 'Activewear', href: '#' },
-            { name: 'Browse All', href: '#' },
-          ],
-        },
-        {
-          id: 'accessories',
-          name: 'Accessories',
-          items: [
-            { name: 'Watches', href: '#' },
-            { name: 'Wallets', href: '#' },
-            { name: 'Bags', href: '#' },
-            { name: 'Sunglasses', href: '#' },
-            { name: 'Hats', href: '#' },
-            { name: 'Belts', href: '#' },
-          ],
-        },
-        {
-          id: 'brands',
-          name: 'Brands',
-          items: [
-            { name: 'Full Nelson', href: '#' },
-            { name: 'My Way', href: '#' },
-            { name: 'Re-Arranged', href: '#' },
-            { name: 'Counterfeit', href: '#' },
-            { name: 'Significant Other', href: '#' },
-          ],
-        },
-      ],
-    },
-    {
-      id: 'about_us',
-      name: 'About us',
-      featured: [
-        {
-          name: 'New Arrivals',
-          href: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
-          imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
-        },
-        {
-          name: 'Artwork Tees',
-          href: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg',
-          imageAlt:
-            'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
-        },
-      ],
-      sections: [
-        {
-          id: 'clothing',
-          name: 'Clothing',
-          items: [
-            { name: 'Tops', href: '#' },
-            { name: 'Pants', href: '#' },
-            { name: 'Sweaters', href: '#' },
-            { name: 'T-Shirts', href: '#' },
-            { name: 'Jackets', href: '#' },
-            { name: 'Activewear', href: '#' },
-            { name: 'Browse All', href: '#' },
-          ],
-        },
-        {
-          id: 'accessories',
-          name: 'Accessories',
-          items: [
-            { name: 'Watches', href: '#' },
-            { name: 'Wallets', href: '#' },
-            { name: 'Bags', href: '#' },
-            { name: 'Sunglasses', href: '#' },
-            { name: 'Hats', href: '#' },
-            { name: 'Belts', href: '#' },
-          ],
-        },
-        {
-          id: 'brands',
-          name: 'Brands',
-          items: [
-            { name: 'Re-Arranged', href: '#' },
-            { name: 'Counterfeit', href: '#' },
-            { name: 'Full Nelson', href: '#' },
-            { name: 'My Way', href: '#' },
-          ],
-        },
-      ],
+      href: '/',
     },
   ],
   pages: [
-    { name: 'Support', href: '#' }
+    { name: 'Support', href: '#' },
+    { name: 'About us', href: '/about-us' }
   ],
 }
 
@@ -154,12 +48,13 @@ export default function NavBar() {
   const [cartItems, setCartItems] = useState<Item[]>([])
   const itemState = useCartStore(state => state.items);
   const getBannerText = trpc.products.getBannerText.useQuery()
+  const router = useRouter()
 
 
   useEffect(() => {
     setCartItems(itemState)
   }, [itemState])
-  
+
   return (
     <>
       <div className="bg-white">
@@ -213,6 +108,7 @@ export default function NavBar() {
                                 'flex-1 whitespace-nowrap border-b-2 py-4 px-1 text-base font-medium'
                               )
                             }
+                            onClick={() => { router.push(category.href) }}
                           >
                             {category.name}
                           </Tab>
@@ -223,41 +119,9 @@ export default function NavBar() {
                       {navigation.categories.map((category) => (
                         <Tab.Panel key={category.name} className="space-y-10 px-4 pt-10 pb-8">
                           <div className="grid grid-cols-2 gap-x-4">
-                            {category.featured.map((item) => (
-                              <div key={item.name} className="group relative text-sm">
-                                <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
-                                  <img src={item.imageSrc} alt={item.imageAlt} className="object-cover object-center" />
-                                </div>
-                                <a href={item.href} className="mt-6 block font-medium text-gray-900">
-                                  <span className="absolute inset-0 z-10" aria-hidden="true" />
-                                  {item.name}
-                                </a>
-                                <p aria-hidden="true" className="mt-1">
-                                  Shop now
-                                </p>
-                              </div>
-                            ))}
+
                           </div>
-                          {category.sections.map((section) => (
-                            <div key={section.name}>
-                              <p id={`${category.id}-${section.id}-heading-mobile`} className="font-medium text-gray-900">
-                                {section.name}
-                              </p>
-                              <ul
-                                role="list"
-                                aria-labelledby={`${category.id}-${section.id}-heading-mobile`}
-                                className="mt-6 flex flex-col space-y-6"
-                              >
-                                {section.items.map((item) => (
-                                  <li key={item.name} className="flow-root">
-                                    <a href={item.href} className="-m-2 block p-2 text-gray-500">
-                                      {item.name}
-                                    </a>
-                                  </li>
-                                ))}
-                              </ul>
-                            </div>
-                          ))}
+
                         </Tab.Panel>
                       ))}
                     </Tab.Panels>
@@ -343,7 +207,7 @@ export default function NavBar() {
                 {/* Logo */}
                 <div className="ml-4 flex lg:ml-0">
                   <Link href="/">
-                   <span className="text-3xl font-normal text-black">Hustlermy</span>
+                    <span className="text-3xl font-normal text-black">Hustlermy</span>
                   </Link>
                 </div>
 
@@ -384,46 +248,10 @@ export default function NavBar() {
                                   <div className="mx-auto max-w-7xl px-8">
                                     <div className="grid grid-cols-2 gap-y-10 gap-x-8 py-16">
                                       <div className="col-start-2 grid grid-cols-2 gap-x-8">
-                                        {category.featured.map((item) => (
-                                          <div key={item.name} className="group relative text-base sm:text-sm">
-                                            <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
-                                              <img
-                                                src={item.imageSrc}
-                                                alt={item.imageAlt}
-                                                className="object-cover object-center"
-                                              />
-                                            </div>
-                                            <a href={item.href} className="mt-6 block font-medium text-gray-900">
-                                              <span className="absolute inset-0 z-10" aria-hidden="true" />
-                                              {item.name}
-                                            </a>
-                                            <p aria-hidden="true" className="mt-1">
-                                              Shop now
-                                            </p>
-                                          </div>
-                                        ))}
+
                                       </div>
                                       <div className="row-start-1 grid grid-cols-3 gap-y-10 gap-x-8 text-sm">
-                                        {category.sections.map((section) => (
-                                          <div key={section.name}>
-                                            <p id={`${section.name}-heading`} className="font-medium text-gray-900">
-                                              {section.name}
-                                            </p>
-                                            <ul
-                                              role="list"
-                                              aria-labelledby={`${section.name}-heading`}
-                                              className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
-                                            >
-                                              {section.items.map((item) => (
-                                                <li key={item.name} className="flex">
-                                                  <a href={item.href} className="hover:text-gray-800">
-                                                    {item.name}
-                                                  </a>
-                                                </li>
-                                              ))}
-                                            </ul>
-                                          </div>
-                                        ))}
+
                                       </div>
                                     </div>
                                   </div>
@@ -449,7 +277,7 @@ export default function NavBar() {
 
                 <div className="ml-auto flex items-center">
                   <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                  
+
                   </div>
                   {/* Search */}
                   <div className="flex lg:ml-6">
@@ -477,8 +305,8 @@ export default function NavBar() {
           </nav>
         </header>
       </div>
-      <SideShoppingCart cartOpen={cartOpen} setOpen={setOpen} setCartOpen={setCartOpen}/>
-   
+      <SideShoppingCart cartOpen={cartOpen} setOpen={setOpen} setCartOpen={setCartOpen} />
+
     </>
   )
 }
