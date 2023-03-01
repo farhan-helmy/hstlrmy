341a50a0cad9b1e9da1f0c45faf9c3f119778583
farhan <farhanhlmy@gmail.com>
2023-03-02 00:03:34 +0800

    chore: remove unused custom hook file

--- src/custom-hooks/index.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export function useOutsideClick(ref: any, onClickOut: () => void) {
-  useEffect(() => {
-    const onClick = ({ target }: any) => !ref.contains(target) && onClickOut?.()
-    document.addEventListener("click", onClick);
-    return () => document.removeEventListener("click", onClick);
-  }, []);
-}
